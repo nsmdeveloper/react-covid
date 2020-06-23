a1a4ddf35254a88879d7647ded20ffdff5bc8a36
Natiums <devnatanael@gmail.com>
2020-06-22 20:22:52 -0400

    Algunos cambios

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Covid Results</title>
+  <link rel="stylesheet" href="./css/bootstrap.min.css" 
+type="text/css">
+
+<link rel="manifest" href="./manifest.json">
+
+  <script src="./js/jquery-3.3.1.slim.min.js" type="text/javascript"></script>
+
+  <script src="./js/popper.min.js" type="text/javascript"></script>
+
+  <script src="./js/bootstrap.min.js" 
+ type="text/javascript"></script>
+
+  <script src="./js/react.production.min.js" type="text/javascript"></script>
+  <script src="./js/react-dom.production.min.js" type="text/javascript"></script>
+   <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+   <script src="./js/main.js" type="text/javascript"></script>
+
+</head>
+  <body>
+  
+    <div id="root"></div>
+    <script type="text/babel" src="covid.js"></script>
+  </body>
+</html>
